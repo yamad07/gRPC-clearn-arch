38b449a6dfe1fbfe9365f37423c25810e98d44a2
Yusuke Yamada <yuhsukeshootsfc@gmail.com>
2019-04-10 00:46:04 +0900

    [fix] Change environment variable

--- infra/db.go
+++ infra/db.go
@@ -11,13 +11,13 @@ import (
 var DB *gorm.DB
 
 func Init() {
-	user := getParamFromEnv("EMODE_DB_USER", "root")
-	host := getParamFromEnv("EMODE_DB_HOST", "127.0.0.1")
-	password := getParamFromEnv("EMODE_DB_PASSWORD", "")
-	name := getParamFromEnv("EMODE_DB_NAME", "e_mode_dev")
-	protocol := getParamFromEnv("EMODE_DB_PROTOCOL", "tcp")
-	port := getParamFromEnv("EMODE_DB_PORT", "3306")
-	args := getParamFromEnv("EMODE_DB_ARGS", "parseTime=true&loc=Asia%2FTokyo&charset=utf8mb4")
+	user := getParamFromEnv("TODO_DB_USER", "root")
+	host := getParamFromEnv("TODO_DB_HOST", "127.0.0.1")
+	password := getParamFromEnv("TODO_DB_PASSWORD", "")
+	name := getParamFromEnv("TODO_DB_NAME", "e_mode_dev")
+	protocol := getParamFromEnv("TODO_DB_PROTOCOL", "tcp")
+	port := getParamFromEnv("TODO_DB_PORT", "3306")
+	args := getParamFromEnv("TODO_DB_ARGS", "parseTime=true&loc=Asia%2FTokyo&charset=utf8mb4")
 	connectionString := fmt.Sprintf("%s:%s@%s([%s]:%s)/%s?%s",
 		user, password, protocol, host, port, name, args)
 
